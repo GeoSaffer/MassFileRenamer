8aa19181054427cd03561c990b2adaee3c9f1cb4
GeoSaffer <dfouche8@gmail.com>
2019-09-04 09:13:16 +1200

    Add Project to Source Control

---
+++ MassFileRenamer/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace MassFileRenamer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtOldStr = new System.Windows.Forms.TextBox();
+            this.txtNewStr = new System.Windows.Forms.TextBox();
+            this.btnRename = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtFolder = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtOldStr
+            // 
+            this.txtOldStr.Location = new System.Drawing.Point(107, 78);
+            this.txtOldStr.Name = "txtOldStr";
+            this.txtOldStr.Size = new System.Drawing.Size(187, 20);
+            this.txtOldStr.TabIndex = 0;
+            this.txtOldStr.MouseEnter += new System.EventHandler(this.txtOldStr_MouseEnter);
+            this.txtOldStr.MouseHover += new System.EventHandler(this.txtOldStr_MouseHover);
+            // 
+            // txtNewStr
+            // 
+            this.txtNewStr.Location = new System.Drawing.Point(107, 111);
+            this.txtNewStr.Name = "txtNewStr";
+            this.txtNewStr.Size = new System.Drawing.Size(187, 20);
+            this.txtNewStr.TabIndex = 1;
+            this.txtNewStr.MouseEnter += new System.EventHandler(this.txtNewStr_MouseEnter);
+            this.txtNewStr.MouseHover += new System.EventHandler(this.txtNewStr_MouseHover);
+            // 
+            // btnRename
+            // 
+            this.btnRename.Location = new System.Drawing.Point(117, 153);
+            this.btnRename.Name = "btnRename";
+            this.btnRename.Size = new System.Drawing.Size(75, 23);
+            this.btnRename.TabIndex = 2;
+            this.btnRename.Text = "Rename";
+            this.btnRename.UseVisualStyleBackColor = true;
+            this.btnRename.Click += new System.EventHandler(this.btnRename_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Unwanted Text";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "New Text";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Select Folder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtFolder
+            // 
+            this.txtFolder.Location = new System.Drawing.Point(117, 49);
+            this.txtFolder.Name = "txtFolder";
+            this.txtFolder.Size = new System.Drawing.Size(177, 20);
+            this.txtFolder.TabIndex = 6;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(309, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(309, 193);
+            this.Controls.Add(this.txtFolder);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnRename);
+            this.Controls.Add(this.txtNewStr);
+            this.Controls.Add(this.txtOldStr);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Mass File Renamer";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtOldStr;
+        private System.Windows.Forms.TextBox txtNewStr;
+        private System.Windows.Forms.Button btnRename;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtFolder;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+    }
+}
+
---
+++ MassFileRenamer/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+//using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MassFileRenamer
+{
+    public partial class Form1 : Form
+    {
+        string oldStr, newStr;
+        System.Windows.Forms.ToolTip ToolTip1 = new System.Windows.Forms.ToolTip();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void get_filenames()
+        {
+            DirectoryInfo diDirectory;
+            FileInfo[] fiFiles;
+            int i;
+
+            diDirectory = new DirectoryInfo(txtFolder.Text + @"\");
+            fiFiles = diDirectory.GetFiles();
+
+            if (fiFiles != null)
+            {
+               // MessageBox.Show(txtFolder.Text + @"\");
+                for (i = 0; i < fiFiles.Length; i++)
+                {
+                   // MessageBox.Show(fiFiles[i].Name.ToString());
+                    try
+                    {
+                        File.Move(txtFolder.Text + @"\" + fiFiles[i].Name.ToString(), txtFolder.Text + @"\" + fiFiles[i].Name.Replace(txtOldStr.Text, txtNewStr.Text).ToString());
+                    }
+                    catch (Exception ex)
+                    {
+                        // throw;
+                        MessageBox.Show("Multiple files of the same name can not exist !");
+                    }
+                }
+            }
+        }
+
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            txtFolder.Text = folderBrowserDialog1.SelectedPath;
+        }
+
+        private void btnRename_Click(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+            get_filenames();
+            Cursor.Current = Cursors.Default;
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutBox1 about = new AboutBox1();
+            about.ShowDialog();
+        }
+
+        private void txtOldStr_MouseHover(object sender, EventArgs e)
+        {
+            
+            
+            ToolTip1.IsBalloon = true;
+            ToolTip1.SetToolTip(this.txtOldStr, "The text that you would like to replace.");
+            ToolTip1.ShowAlways = true;
+
+            ToolTip1.Active = true;
+        }
+
+        private void txtNewStr_MouseHover(object sender, EventArgs e)
+        {
+            ToolTip1.IsBalloon = true;
+            ToolTip1.SetToolTip(this.txtNewStr, "The text that you would like to replace the above text.");
+            ToolTip1.ShowAlways = true;
+            ToolTip1.Active = true;
+        }
+
+        private void txtOldStr_MouseEnter(object sender, EventArgs e)
+        {
+            ToolTip1.Active = false;
+        }
+
+        private void txtNewStr_MouseEnter(object sender, EventArgs e)
+        {
+            ToolTip1.Active = false;
+        }
+        
+    }
+}
